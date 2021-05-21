4f7df749bb5f527e6ab2478e85514878befce5af
andrebiann <45128990+andrebiann@users.noreply.github.com>
2021-05-20 23:37:52 -0300

    Commit_Atom
    Blog_Atom_Commit

---
+++ blog/blog/index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Praticando Arrays <br>";
+
+$nome = "Maria";
+$idade = 30;
+$peso = 1,7;
+$fumante = True;
+
+$cadastro = array("Maria da Silva", 30, 1.7, true);
+
+
+
---
+++ blog/index.php
@@ -0,0 +1,32 @@
+<?php
+
+echo "Praticando Arrays <br>";
+echo "Praticando com HEROKU <br>";
+echo "Praticando com HEROKU <br>";
+
+$Cadastro = array("Maria da Silva", true, "Olá", false);
+$cadastro = array ("nome" => "Maria da Silva", "idade=>30","altura"=1.7, "fumante"=>  true);
+
+
+var_dump($Cadastro);
+//var_dump($cadastro);
+
+// Continua...
+
+
+for ($i=0; $i < ; $i++) {
+
+}
+
+// Continua...
+
+//var_dump($Cadastro);
+//var_dump($cadastro);
+
+$tamanho = count($Cadastro);
+//echo $tamanho;
+
+for ($i=0; $i < 2; $i++) {
+    echo $ $Cadastro[$i];
+    echo "<br>";
+}
